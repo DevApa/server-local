db26fd4cc6f341c0d235b300e8e76883953151b2
Carlos Aparicio <apariciovivero@gmail.com>
2024-06-05 09:26:34 -0500

    Adaptacion de exportaciones de manera moderna

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 //funcion agnostica auto convocada
 //agnosticas porque no posee nombre y auto convocada por los ()
 //schema ( () => {})()
-const { envs } = require("./config/env");
-const { startServer } = require("./server/server");
+//const { startServer } = require("./server/server");
+import { startServer } from "./server/server.js";
+//const { envs } = require("./config/env.js");
+import { envs } from "./config/env.js";
+
+
 
 const main=()=>{
     console.log("Función principal")    
     console.log(envs)
-    startServer({
-        port: envs.PORT,
-        public_path: envs.PUBLIC_PATH
-    })
+    // startServer({
+    //     port: envs.PORT,
+    //     public_path: envs.PUBLIC_PATH
+    // })
 }
 
 //funcion asincrona
